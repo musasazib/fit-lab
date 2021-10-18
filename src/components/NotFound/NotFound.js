f0ed13c4aa752f3b9d6fb5950ab9949a2bdb6c6b
musasazib <musasazib@gmail.com>
2021-10-19 04:55:25 +0600

    Page Not found component finish

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div className="not-found-container">
             <h1 className="error">Error Page</h1>
-            <div className="error-text">
+            <div className="error-text-container">
                 <h1>404</h1>
                 <h2>Page Not Found</h2>
                 <p>We’re unable to find a page you are looking for, Try later or click the button.</p>
