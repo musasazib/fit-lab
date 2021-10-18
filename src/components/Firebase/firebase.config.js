d547652683febf6149c1da8991c02498aa0b4847
musasazib <musasazib@gmail.com>
2021-10-19 00:18:44 +0600

    Create & added firebase

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4adK710esWYcTRscBleB7rZ9Rgc0uixg",
+    authDomain: "fit-lab-dhaka.firebaseapp.com",
+    projectId: "fit-lab-dhaka",
+    storageBucket: "fit-lab-dhaka.appspot.com",
+    messagingSenderId: "849135851690",
+    appId: "1:849135851690:web:62caab9ef3a27b22f918d3"
+};
+
+export default firebaseConfig;
