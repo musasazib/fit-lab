4fdbabccaa37a49a634801c48caf30f4b5054e1a
musasazib <musasazib@gmail.com>
2021-10-19 21:58:54 +0600

    Header done

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import logo from '../../../images/logo.png';
@@ -22,15 +22,15 @@ const Header = () => {
                         <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
                         <Nav.Link as={Link} to="/package">Package</Nav.Link>
                         <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        {user.email ?
+                        <Button onClick={logOut} className="logout-btn" as={Link} to="/">Logout</Button>:
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>}
 
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
 
-                        <Nav.Link onClick={logOut} as={Link} to="/">Logout</Nav.Link>
                         <Navbar.Text>
-                            Signed in as: <a href="#login">{user?.displayName}</a>
+                            Signed in as:  <a className="user-name" href="#login">{user?.displayName}</a>
                         </Navbar.Text>
-                        
+
                     </Navbar.Collapse>
                     <Nav className="me-auto">
 
