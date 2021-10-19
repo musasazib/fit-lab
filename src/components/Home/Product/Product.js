2fd6672c8a83631438acd8d91d5f3b07bcf73f3c
musasazib <musasazib@gmail.com>
2021-10-19 20:04:17 +0600

    Produts done

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Product.css';
 
 const Product = ({product}) => {
     const { img, name, price } = product;
     return (
-        <div className="service pb-3">
+        <div className="service-product pb-3">
             <img src={img} alt="" />
-            <h3>{name}</h3>
+            <h3 className="name">{name}</h3>
             <h1>{price}</h1>
-            <button className="btn btn-warning">Add to Cart</button>
+            <button className="btn btn-product">Add to Cart</button>
         </div>
     );
 };
--- src/components/Home/Products/Products.js
+++ src/components/Home/Products/Products.js
@@ -11,7 +11,7 @@ const Products = () => {
     }, [])
     return (
         <div>
-            <h2 className="text-primary mt-5">CHOOSE YOUR PRODUCT AND BOOST YOUR BODY STRENGTH</h2>
+            <h2 className="text-color mt-5 text-center">CHOOSE YOUR PRODUCT AND BOOST YOUR BODY STRENGTH</h2>
             <div className="product-container">
                 {
                     products.map(product => <Product
